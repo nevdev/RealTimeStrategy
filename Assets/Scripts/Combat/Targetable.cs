e6d77fce0b0f3da3997f92b2d4378bb7a5f04ad2
nevdevmak <nevdevmak@gmail.com>
2021-08-21 18:00:43 +0200

    Created AimAtPoint Object,  Targeter & Targetable scripts. Non Network scripts

---
+++ Assets/Scripts/Combat/Targetable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targetable : MonoBehaviour
+{
+    [SerializeField] private Transform aimAtPoint = null;
+
+    private Transform GetAimAtPoint()
+    {
+        return aimAtPoint;
+    }
+}
