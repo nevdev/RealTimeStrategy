3ddb151144c9e554a3e4e281a609422c03f4e9a9
nevdevmak <nevdevmak@gmail.com>
2021-09-08 09:33:00 +0200

    31. Buidings Setups - Layers &   Healthbars facing camera

---
+++ Assets/Scripts/Cameras/FaceCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform mainCameraTransform;
+    private void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+    }
+
+        
+    // makes the Health Bar continously facing the camera
+    private void LateUpdate()
+    {// makes an UI facing the camera when the object of the UI can move around and spins
+        transform.LookAt(
+            transform.position + mainCameraTransform.rotation * Vector3.forward,
+            mainCameraTransform.rotation * Vector3.up
+            );
+    }
+}
